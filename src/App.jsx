d81d5759b5cf5140d2a8e3efd6c839f02e1a3539
shiaiwen <2474577454@qq.com>
2022-12-26 09:45:00 +0800

    合并components和container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import Count from './containers/Count'
-import store from './redux/store'
 
 export default class App extends Component {
   render() {
     return <div>
       {/*  传递 store */}
-      <Count store= { store } />
+      <Count  />
     </div>
   }
 }
--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -1,7 +1,5 @@
-// 引入 CountUI 组件
-import CountUI from '../../components/Count'
-// 引入 store
-// 引入 connect 用于连接 UI 组件与 redux
+import React, { Component } from 'react'
+import './index.css'
 import { connect } from 'react-redux'
 // 创建并暴露一个 Count 的容器组件
 // a 函数的返回值作为状态 传递 给UI 组件
@@ -11,24 +9,60 @@ import {
   createDecrementAction,
   createIncrementAsyncAction
 } from '../../redux/count_action'
-// const mapStateToProps = state => ({ count: state })
-// const mapDispatchToProps = dispatch => ({
-//   increse: data => dispatch(createIncrementAction(data)),
-//   decrese: data => dispatch(createDecrementAction(data)),
-//   increseAsync: (data, time) => dispatch(createIncrementAsyncAction(data, time))
-// })
-
-// dispatch => ({
-//   increse: data => dispatch(createIncrementAction(data)),
-//   decrese: data => dispatch(createDecrementAction(data)),
-//   increseAsync: (data, time) => dispatch(createIncrementAsyncAction(data, time))
-// })
-
-export default connect(
-  state => ({ count: state }),
-  {
-    increse: createIncrementAction,
-    decrese: createDecrementAction,
-    increseAsync: createIncrementAsyncAction
+class Count extends Component {
+  // 方法写在 state 同级
+  increment = () => {
+    // 获取用户要加的值
+    // 1. 修改状态
+    const { value } = this.selectNumber
+    this.props.increse(value * 1)
   }
-)(CountUI)
+
+  decrement = () => {
+    const { value } = this.selectNumber
+    this.props.decrese(value * 1)
+  }
+  // 奇数的时候再加
+  incrementIfOdd = () => {
+    // 获取用户要加的值
+    const { value } = this.selectNumber
+    const { count } = this.props
+    // 获取初始的值
+    if (count % 2 !== 0) {
+      this.props.increse(value * 1)
+    }
+  }
+  incrementAsync = () => {
+    // 获取用户要加的值
+    const { value } = this.selectNumber
+    this.props.increseAsync(value * 1, 500)
+  }
+
+  render() {
+    console.log('UI组件接收到的props是', this.props)
+    const { count } = this.props
+    return (
+      <div>
+        <h1>当前求和为: {count}</h1>
+        <select ref={c => (this.selectNumber = c)}>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+        </select>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.incrementIfOdd}>当前求和为奇数的时候在加</button>
+        <button onClick={this.incrementAsync}>异步加</button>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default connect(state => ({ count: state }), {
+  increse: createIncrementAction,
+  decrese: createDecrementAction,
+  increseAsync: createIncrementAsyncAction
+})(Count)
